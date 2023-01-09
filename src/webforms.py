b21e4b32896b9e0d5bb65c652f086962f12609e5
pykrete <landysixten@gmail.com>
2023-01-09 18:57:39 +0100

    Update webforms.py

--- src/webforms.py
+++ src/webforms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, DateField, BooleanField
 from wtforms.validators import DataRequired
 
 class SignUpForm(FlaskForm):
@@ -13,3 +13,13 @@ class LoginForm(FlaskForm):
 class UpdateForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     submit = SubmitField('Update account')
+
+class ReservationForm(FlaskForm):
+    amount_of_guests = StringField('Amount of guests', validators=[DataRequired()])
+    start_date = DateField('Start date', validators=[DataRequired()])
+    end_date = DateField('End date', validators=[DataRequired()])
+    submit = SubmitField('Continue')
+
+class ConfirmForm(FlaskForm):
+    pay = BooleanField('Would you like to pay now? Reservation will be removed if not paid within 10 days.')
+    submit = SubmitField('Confirm')
