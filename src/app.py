f01eb0ae563f6564d0de6e8582c750ba80ec8200
pykrete <landysixten@gmail.com>
2023-01-09 01:43:20 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,11 +1,9 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash, get_flashed_messages, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, login_manager, login_required, logout_user, current_user
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from webforms import SignUpForm, LoginForm, UpdateForm
 
 # Environment variables
 load_dotenv()
@@ -44,19 +42,18 @@ class Reservation(database.Model):
 with app.app_context():
     database.create_all()
 
-# Forms
-class SignUpForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 # App routes
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -69,9 +66,67 @@ def signup():
             user = User(name=form.name.data)
             database.session.add(user)
             database.session.commit()
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Account already exists.')
         name = form.name.data
         form.name.data = ''
     return render_template('signup.html', name=name, form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(name=form.name.data).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Could not find account - Try again.')
+    return render_template('login.html', form=form)
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    form = UpdateForm()
+    id = current_user.id
+    user_to_update = User.query.get_or_404(id)
+    if request.method == 'POST':
+        user_to_update.name = request.form['name']
+        try:
+            database.session.commit()
+            flash('Account successfully updated.')
+            return render_template('dashboard.html', form=form, id=id, user_to_update=user_to_update)
+        except:
+            flash('Something went wrong, try again.')
+            return render_template('dashboard.html', form=form, id=id, user_to_update=user_to_update)
+    else:
+        render_template('dashboard.html', form=form, id=id, user_to_update=user_to_update)
+    return render_template('dashboard.html', form=form, id=id, user_to_update=user_to_update)
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete(id):
+    if id == current_user.id:
+        user = User.query.get_or_404(id)
+        try:
+            database.session.delete(user)
+            database.session.commit()
+            flash('Your account has been successfully deleted.')
+            return redirect(url_for('index'))
+        except:
+            flash('An error occured, please try again.')
+    else:
+        flash("You can't delete that user.")
+        return redirect(url_for('dashboard'))
 
 # Start app
 app.run()
